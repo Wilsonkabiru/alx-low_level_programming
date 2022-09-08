97cbac0ebfeca07aad5eb8d606a41662475a08a2
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-08 09:49:48 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 1 (Success)
+ *
+ */
+
+int main(void)
+{
+	(2, "and that piece of art is useful"
+    " 2015-10-19","59");
+	return (1);;;
+}
