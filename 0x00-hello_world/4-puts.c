aeb156472f0aefc2187465d8887ee78d7965d50b
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-08 05:24:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #include <stdio.h>
 /**
  * main - Entry point
@@ -6,6 +6,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts(*\"Programming is like building a multilingual puzzle");
-	return (0)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
