dd79f102b7086b008a3cc54832712cda71a08e17
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 03:38:34 -0700

    Positive is better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,9 @@
 
 /**
  * main - print if the number is positive, zero, or negative
- *
  * Description: using the main function
- * 
  * this program prints "Programming is positive, zero, or negative
- *
  * Return: 0
- *
  */
 int main(void)
 	{
@@ -26,7 +22,7 @@ int main(void)
 	{
 		printf("%i is zero\n", n);
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
 		printf("%i is negative\n", n);
 	}
