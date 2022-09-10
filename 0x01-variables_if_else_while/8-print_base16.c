fa9c968c18995be1466bbd884411e1ad41e6ffaf
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 08:21:02 -0700

    Prints numbers in the base of 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,20 +7,19 @@
 int main(void)
 
 {
-char base_16;
-
-for(base_16 = 48; base_16 <= 57; base_16++)
+	char base_16;
+	
+	for (base_16 = 48; base_16 <= 57; base_16++)
 	{
-	putchar(base_16);
+		putchar(base_16);
+	}
+	
+	for (base_16 = 97; base_16 <= 102; base_16++)
+	{
+		putchar(base_16);
 	}
 
-for (base_16 = 97; base_16 <= 102; base_16++)
-	{
-	putchar(base_16);
-	}
+	putchar('\n');
 
-putchar('\n');
-
-return (0);
-
+	return (0);
 }
