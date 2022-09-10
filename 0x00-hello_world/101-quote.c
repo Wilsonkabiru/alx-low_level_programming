57159bc08263cf268fab3ca64113862c20bf4638
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 02:08:58 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <string.h>
 #include <unistd.h>
 
 /**
  * main -Prints "and that piece of art is useful"-Dora Korpar, 2015-10-19,
  *followed by a new line, to the standard error.
  * Return:Always 0 (Success)
- *Function Prototypes 
- *int string_length(char *pointer);
- *char* reverse(char *string);
  */
 int main(void)
 {
