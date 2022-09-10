f3ab181ebf1317d60172eb011eb6a9d4a4c79482
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 02:06:34 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
- * main -Entry point
- * "Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
+ * main -Prints "and that piece of art is useful"-Dora Korpar, 2015-10-19,
+ *followed by a new line, to the standard error.
  * Return:Always 0 (Success)
+ *Function Prototypes 
+ *int string_length(char *pointer);
+ *char* reverse(char *string);
  */
 int main(void)
 {
-	int___fd(2,"and that piece of art is useful\"
-	"Dora Korpar, 2015-10-19",59);
-	return (1);
+	write(2,
+	  "(and that piece of art is useful)"
+	 "Dora Korpar, 2015-10-19",59);"";
+	return (1);;
 }
