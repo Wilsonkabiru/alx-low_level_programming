51e08d50c18c813812cefaff8e8323eea33baacd
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 02:52:03 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main -prints the size of various types
- * Return:0 if exited properly, non-zero otherwise
+ * main -  Prints a text
+ *
+ * Return:Always 1 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %li byte(s)\n", sizeof(char));
-	printf("Size of an int: %li byte(s)\n", sizeof(int));
-	printf("Size of a long int:%li byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%li byte(s)\n", sizeof(long long int));
-	printf("Size of a float:%li byte(s)\n", sizeof(float));
-	return (0);;
+	write(2,
+		"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
