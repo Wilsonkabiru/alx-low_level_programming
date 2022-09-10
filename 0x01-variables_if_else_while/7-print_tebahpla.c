056c6b1f50cfb97e498b0a7a65c91dce3e6fb51f
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-10 07:26:55 -0700

    prints lowercase alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-	char alphabet;
-	for (alphabet = 122 ; alphabet >= 97; alphabet--)
+char alphabet;
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
 	{
 		putchar(alphabet);
 	}
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
