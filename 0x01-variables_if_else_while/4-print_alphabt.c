fcfc2f92bf579845e9049bafd659c5f5b8487e18
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-12 10:26:36 -0700

    Print alphabts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- *  main - print if the number is positive, zero, or negative
+ *  main - Entry point
  *
- *  Description: using the main function
- *  this program prints "Programming is positive, zero, or negative"
- *  Return: 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char ch;
-for (ch ='a' ; ch <= 'z' ; ch++)
-{
-if (ch != 'e' $$ ch != 'q');
-{
-	putchar(ch);
-}
-}	
-putchar('\n');
-return (0);
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
 
