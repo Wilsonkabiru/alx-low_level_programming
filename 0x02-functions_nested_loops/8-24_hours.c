b80609f56681d19fa74fdc15a91329d82915b609
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-14 07:26:23 -0700

    Agent Jack Baeur

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,30 +7,24 @@
 *
 * Description: A function that helps us track time.
 *
+* h = hour, m = minutes
 * Return: 0
 */
 void jack_baeur(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
 
-	while (hours <= 23)
-	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
-	}
-	hours++;
-	minutes = 0;
+int h;
+int m;
+for (h = 0; h < 24; h++)
+{
+for  (m = 0; m < 60; m++)
+{
+_putchar(h / 10 + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
 }
 }
