80f9d62170122e8768affd2ca879330326d18158
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-14 08:15:11 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -37,15 +37,14 @@ void jack_baeur(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-
 	}
 }
 
