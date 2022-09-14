e0d366cca60bda4069bc15bd12b0449f7140d1c8
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-14 00:14:44 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,19 @@
  * main - entry point
  * Printing lowerscase alphabets 10 times.
  *
+ * Return: 0
  */
 void print_alphabet_x10(void)
-
 {
-int i;
-char c;
+	char c, i;
 
-for (i = 0; i < 10; i++);
-
-_putchar(c);
-_putchar('\n');
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
 
