0adec9afe0b11b858b38325dd0db16a046077f49
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-19 06:31:34 -0700

    0.98 Battery street

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  *reset_to_98(int *n) - check the code*
-  *@n: integer pointer
-  * Description - A function that resets the value of n to 98
-  * Return: Always 0.
-  */
+*reset_to_98(int *n) - check the code*
+*@n: integer pointer
+* Description - A function that resets the value of n to 98
+* Return: Always 0.
+*/
 
 void reset_to_98(int *n)
 {
