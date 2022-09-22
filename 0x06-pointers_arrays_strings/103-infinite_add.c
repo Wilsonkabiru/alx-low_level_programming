c4e005b2d0c1e1adb80935200d7c57af481cc7bd
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-22 06:42:50 -0700

    Infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,7 @@
  * infinite_add - adds two numbers
  * @n1: number one
  * @n2: number two
- * @r: buffer that the function will use
+ * @r: buffer that the function will use to store the result
  * @size_r: buffer size
  * Return: the pointer to dest
  */
