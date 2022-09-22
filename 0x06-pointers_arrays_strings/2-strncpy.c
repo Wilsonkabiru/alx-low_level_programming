588704838d1d91d1431712d21d1273a414040a93
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-22 04:55:36 -0700

    2.strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,6 +9,13 @@
  */
 char *_strcpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
