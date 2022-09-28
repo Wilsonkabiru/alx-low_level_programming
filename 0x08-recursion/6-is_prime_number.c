900cc657aa608397a1a0270e4d8464d652440571
Wilsonkabiru <wkabiru19@gmail.com>
2022-09-27 23:35:42 -0700

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,7 @@
  * helperFunction - returns 0 or 1
  * @num: number being checked
  * @i: possible factor of the number
+ *
  * Return: 0 if not prime, 1 if prime
  */
 int helperFunction(int num, int i)
