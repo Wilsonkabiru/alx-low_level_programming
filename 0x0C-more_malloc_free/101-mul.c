683652192c10528c8f88bfbf34cf5355ca060418
Wilsonkabiru <wkabiru19@gmail.com>
2022-10-06 03:48:05 -0700

    Infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[])
 		argv[1] = iterate_zeroes(argv[1]);
 	if (*(argv[2]) == '0')
 		argv[2] = iterate_zeroes(argv[2]);
-	if (*(argv[1]) == '0' || *(argv[2]) == '\0')
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
 	{
 		printf("0\n");
 		return (0);
